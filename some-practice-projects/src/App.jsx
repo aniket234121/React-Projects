198028e6a2bfc511639203aade2cfa3cec597d04
aniket chauhan <aniketchauhan329@gmail.com>
2024-11-02 11:55:15 +0530

    implemented scroll progress bar

--- some-practice-projects/src/App.jsx
+++ some-practice-projects/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageSlider from "./components/ImageSlider";
 import Products from "./components/Products";
 import TreeView from "./components/TreeView";
@@ -6,8 +6,31 @@ import { TreeData } from "./data";
 import QrCode from "./components/QrCode";
 import ThemeContextProvider from "./components/ThemeContextProvider";
 const App = () => {
+  const [scrollPercentage, setScrollPercent] = useState("0px");
+
+  function handleScroll() {
+    const position =
+      (document.documentElement.scrollTop /
+        (document.documentElement.scrollHeight - window.innerHeight)) *
+      100;
+    setScrollPercent(`${Math.ceil(position)}vh`);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
   return (
     <>
+    <div className={`h-screen fixed left-0 z-20 `}>
+          <div
+            className={` bg-red-500 h-2 absolute w-2 `}
+            style={{ height: scrollPercentage }}
+          >
+            
+          </div>
+        </div>
       <ThemeContextProvider>
         <ImageSlider />
         <div className="flex">
@@ -15,6 +38,7 @@ const App = () => {
           <Products></Products>
         </div>
         <QrCode />
+        
       </ThemeContextProvider>
     </>
   );
